b5182844c42606747d5ad1eac7a984def16b64c0
ashiqurras <ashiqurrussel2@gmail.com>
2021-12-14 05:04:17 +0100

    Applied underline css on Heading

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -1,8 +1,29 @@
 import React, { useState } from 'react';
 import data from './data';
-import List from './List';
+import List from './components/List';
 function App() {
-  return <h2>reminder project setup</h2>;
+
+  //Define state
+  const [people,setPeople] = useState(data);
+
+  //Handler for delete all list
+  const deleteAll = ()=>{
+    setPeople([]);
+  }
+
+  return (
+    <main>
+      <section className='container'>
+      <h2>Birthday Reminder</h2>
+            <h2 className='underline'></h2>
+
+      <h3>{people.length} birthdays today</h3>
+      <List people={people}/> 
+      <button className='btn' onClick={deleteAll}> Clear All</button>
+      </section>
+  
+    </main>
+  )
 }
 
 export default App;
